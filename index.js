f9df06c963d0a47b8889e5750f98f979c3d3d0a4
Janice Smith <janice.k.n.smith@gmail.com>
2018-10-06 16:40:45 -0400

    you can now add and remove selectors from the menu

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var hexColorBank = ["#FF0000", "#EE82EE", "#FF00FF", "#FF69B4", "#FFB6C1", "#FF1493"]
 
 function changeColorByCssSelector(e) {
-    // TODO: find the closest selector value - rather than by Id
-  var selector = document.getElementById("selector-text-input").value;
-  // TODO: find the closest color - rather than by Id
-  var color = document.getElementById("color-preview").value;
+  e.target.closest(".selector-text-input");
+  var selector = e.target.parentNode.querySelector(".selector-text-input").value;
+  var color = e.target.parentNode.querySelector(".color-preview").value;
   var codeToUpdateColor = "document.querySelector('" + selector + "').style.backgroundColor='" + color + "'";
   chrome.tabs.executeScript(null,
       {code: codeToUpdateColor}
@@ -12,7 +11,7 @@ function changeColorByCssSelector(e) {
 }
 
 function removeSelector(e) {
-  var selector = document.getElementById("selector-text-input").value;
+  var selector = e.target.parentNode.querySelector(".selector-text-input").value;
   var codeToUpdateColor = "document.querySelector('" + selector + "').style.backgroundColor='inherit'";
   chrome.tabs.executeScript(null,
       {code: codeToUpdateColor}
@@ -20,9 +19,48 @@ function removeSelector(e) {
   e.target.parentNode.remove();
 }
 
+function selectorElements() {
+  var selectorEleWrap = document.createElement("div");
+  selectorEleWrap.setAttribute("class", "selector");
+
+  var colorInput = document.createElement("input");
+  colorInput.type = "color";
+  colorInput.setAttribute("class", "color-preview");
+  // set a random color from the hex bank
+  colorInput.value = hexColorBank[Math.floor(Math.random() * hexColorBank.length)];
+  selectorEleWrap.appendChild(colorInput);
+
+  var textInput = document.createElement("input");
+  textInput.type = "text";
+  textInput.setAttribute("class", "selector-text-input");
+  selectorEleWrap.appendChild(textInput);
+
+  var removerEle = document.createElement("button");
+  removerEle.innerHTML = "x";
+  removerEle.setAttribute("class", "remove-selector-button");
+  selectorEleWrap.appendChild(removerEle);
+  removerEle.addEventListener("click", removeSelector);
+
+  var applyColorEle = document.createElement("button");
+  applyColorEle.innerHTML = "apply";
+  applyColorEle.setAttribute("class", "apply-color-button");
+  selectorEleWrap.appendChild(applyColorEle);
+  applyColorEle.addEventListener("click", changeColorByCssSelector);
+
+  return selectorEleWrap;
+}
+
+function addSelector() {
+  var selectorInputElements = selectorElements();
+  document.getElementById("selector-container").insertAdjacentElement("beforeend", selectorInputElements);
+}
+
+
 document.addEventListener("DOMContentLoaded", function() {
+  
+  document.querySelector(".apply-color-button").addEventListener("click", changeColorByCssSelector);
 
-  document.getElementById("apply-color-button").addEventListener("click", changeColorByCssSelector);
+  document.querySelector(".remove-selector-button").addEventListener("click", removeSelector);
 
-  document.getElementById("remove-selector-button").addEventListener("click", removeSelector);
+  document.getElementById("add-selector-input").addEventListener("click", addSelector);
 });
